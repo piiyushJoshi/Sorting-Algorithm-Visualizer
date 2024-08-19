f3c8f169629af332567e8a06927ee05eaa13c7c0
Piyush Joshi <122557996+piiyushJoshi@users.noreply.github.com>
2024-08-19 22:37:25 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,15 @@
-
 import time
 
-def bubble_sort(data,drawData,timeTick):
-    for _ in range(len(data)-1):
-        for j in range(len(data)-1):
-            if data[j] > data[j+1]:
-                data[j],data[j+1] = data[j+1],data[j]
-                drawData(data)
+def bubble_sort(data, drawData, timeTick):
+    global comparisons, swaps
+    comparisons = 0
+    swaps = 0
+    for _ in range(len(data) - 1):
+        for j in range(len(data) - 1):
+            comparisons += 1
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+                swaps += 1
+                drawData(data, comparisons, swaps)
                 time.sleep(timeTick)
-
+    drawData(data, comparisons, swaps)
