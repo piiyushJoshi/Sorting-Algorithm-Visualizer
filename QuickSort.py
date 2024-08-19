b2211c3c64140a20536c8e29da4f4871103e8727
Piyush Joshi <122557996+piiyushJoshi@users.noreply.github.com>
2024-08-19 22:39:14 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,28 +1,33 @@
 import time
 
-def partition(data,head,tail,drawData,timeTick):
+comparisons = 0
+swaps = 0
+
+def partition(data, head, tail, drawData, timeTick):
+    global comparisons, swaps
     border = head
     pivot = data[tail]
-
-    drawData(data)
+    drawData(data, comparisons, swaps)
     time.sleep(timeTick)
-
-    for j in range(head,tail):
-        if(data[j] < pivot):
-            drawData(data)
+    for j in range(head, tail):
+        comparisons += 1
+        if data[j] < pivot:
+            drawData(data, comparisons, swaps)
             time.sleep(timeTick)
-            data[border],data[j] = data[j],data[border]
-            border+=1
+            data[border], data[j] = data[j], data[border]
+            swaps += 1
+            border += 1
 
-    drawData(data)
+    drawData(data, comparisons, swaps)
     time.sleep(timeTick)
-    data[border],data[tail] = data[tail],data[border]
+    data[border], data[tail] = data[tail], data[border]
+    swaps += 1
     return border
 
-def quick_sort(data,head,tail,drawData,timeTick):
-    if head < tail:
-        partitionIdx = partition(data,head,tail,drawData,timeTick)
-    
-        quick_sort(data,head,partitionIdx-1,drawData,timeTick)
+def quick_sort(data, head, tail, drawData, timeTick):
 
-        quick_sort(data,partitionIdx+1,tail,drawData,timeTick)
+    if head < tail:
+        partitionIdx = partition(data, head, tail, drawData, timeTick)
+        quick_sort(data, head, partitionIdx - 1, drawData, timeTick)
+        quick_sort(data, partitionIdx + 1, tail, drawData, timeTick)
+
